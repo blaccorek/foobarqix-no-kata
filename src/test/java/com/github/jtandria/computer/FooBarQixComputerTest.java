5aece756b39096e980424aeb54eae860b370a5e3
Tsiorintsoa Andriamihamina <jt.andriamihamina@gmail.com>
2021-11-23 23:48:58 +0100

    Add 0 rule in test

--- src/test/java/com/github/jtandria/computer/FooBarQixComputerTest.java
+++ src/test/java/com/github/jtandria/computer/FooBarQixComputerTest.java
@@ -19,7 +19,7 @@ public class FooBarQixComputerTest {
     static final Logger LOGGER = Logger.getLogger(FooBarQixComputerTest.class.getName());
 
     @ParameterizedTest
-    @CsvSource({ "3, Foo", "5, Bar", "7, Qix" })
+    @CsvSource({ "3, Foo", "5, Bar", "7, Qix", "0, *" })
     void testMatchRuleSuccess(ArgumentsAccessor arguments) {
         final char character = arguments.getCharacter(0);
         final char input = arguments.getCharacter(0);
@@ -71,11 +71,19 @@ public class FooBarQixComputerTest {
         }, "WrongFormatException was expected");
 
         Assertions.assertEquals("Input has wrong format", thrown.getMessage());
+
+        try {
+            final String result = computer.computeDivisibleRule(0, "*", "126");
+            Assertions.assertEquals("", result, "Division rule with has unexpected behaviour");
+        } catch (WrongFormatException | ArithmeticException e) {
+            Assertions.fail("Exception received");
+        }
     }
 
     @ParameterizedTest
     @CsvSource({ "1, 1", "2, 2", "3, FooFoo", "4, 4", "5, BarBar", "6, Foo", "7, QixQix", "8, 8", "9, Foo", "10, Bar",
-            "13, Foo", "15, FooBarBar", "21, FooQix", "33, FooFooFoo", "51, FooBar", "53, BarFoo" })
+            "13, Foo", "15, FooBarBar", "21, FooQix", "33, FooFooFoo", "51, FooBar", "53, BarFoo", "101, 1*1",
+            "303, FooFoo*Foo", "105, FooBarQix*Bar", "10101, FooQix**" })
     void validationTest(ArgumentsAccessor arguments) {
         final String number = arguments.getString(0);
         final String expectedResult = arguments.getString(1);
